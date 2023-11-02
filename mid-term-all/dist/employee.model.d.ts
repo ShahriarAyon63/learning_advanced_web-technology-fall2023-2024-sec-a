5051e107d3debc3f0498b6e1e48bb1e5072ec9ff
Shahriar Ayon <99740018+ShahriarAyon63@users.noreply.github.com>
2023-11-02 15:10:56 +0600

    Delete mid-term-all/dist directory

--- mid-term-all/dist/employee.model.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Employee {
-    name: string;
-    contactNo: string;
-    username: string;
-    password: string;
-    constructor(name: string, contactNo: string, username: string, password: string);
-}
--- mid-term-all/dist/employees.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Employee } from './employee.model';
-export declare class EmployeesService {
-    private employees;
-    registerEmployee(employee: Employee): void;
-    updateEmployee(username: string, newData: Partial<Employee>): boolean;
-    deleteEmployee(username: string): boolean;
-    findEmployee(username: string): Employee;
-    getAllEmployees(): Employee[];
-}
